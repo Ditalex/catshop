a4d39b6d38c88f116965ebae100e2af0f2037be5
Alexa Castro <alexa_castr0@outlook.com>
2025-08-26 15:33:39 -0500

    Vite configuracion

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
@@ -7,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAPEeYGDXeF3iiqj9nTfkIZ6lFO1IEaS_E",
   authDomain: "catshop-ef2f8.firebaseapp.com",
   projectId: "catshop-ef2f8",
-  storageBucket: "catshop-ef2f8.appspot.com", 
+  storageBucket: "catshop-ef2f8.appspot.com",
   messagingSenderId: "1035843976241",
   appId: "1:1035843976241:web:4fc18f0f0592bd2e0b2910",
   measurementId: "G-RXSRPZWM5T"
